0fa5af47de22c355982bc949928ee5a4ee2b41fd
emawby <emawby@gmail.com>
2020-07-30 17:09:09 -0700

    supporting lifecycle observer in unit tests

---
+++ iOS_SDK/OneSignalSDK/UnitTests/Shadows/OSInAppMessageViewControllerOverrider.h
@@ -0,0 +1,12 @@
+//
+//  OSInAppMessageViewControllerOverrider.h
+//  UnitTests
+//
+//  Created by Elliot Mawby on 8/3/20.
+//  Copyright © 2020 Hiptic. All rights reserved.
+//
+
+@interface OSInAppMessageViewControllerOverrider : NSObject
+
+@end
+
